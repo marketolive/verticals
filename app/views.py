454e12f8f891a7fedeb1f60c052a52f012b8b85f
harishparam <hparameswaran@marketo.com>
2016-12-29 14:48:35 -0800

    Added Supporting files

--- app/views.py
+++ app/views.py
@@ -19,9 +19,12 @@ app_dir = os.path.abspath(os.path.dirname(__file__))
 # 		 'customer-base-marketing', 'mobile-marketing', 'higher-education',
 # 		 'financial-services', 'healthcare']
 
-pages = ['base', 'custombase','travel','healthcare','technology','manufacturing',
-		'highered','assetmgmt','financialservices','facebook','economist',
-		'hparameswaran']
+pages = ['base', 'custombase','travel','healthcare','technology','manufacturing','highered','assetmgmt','financialservices','facebook','economist',
+		'search_manufacturing_static','searchasset_static','search_health_care_static','search_higher_ed_static','search_fin_serv_static','search_tech_static','search_travel_static',
+		'New-advances-in-pediatric-healthcare','Encourage-investments-in-long-term-health','Cutting-screening-time-at-hospital',
+		'Marketing-in-Higher-Ed-How-to-Improve-Your-Grades','Higher-Education-A-Changing-World-for-Marketers','How-Digital-Marketing-is-Reshaping-Financial-Services',
+		'Digital-Marketing-Trends','Role_of_Banks_In_Our_Economy','Digital-Marketing-Trends',
+		'Cross-Sell-to-Your-Customers','The-Rise-of-Digital-Marketing-in-Financial-Services']
 
 
 
@@ -55,10 +58,18 @@ def serve_css(file_name):
 def serve_fonts(file_name):
 	return send_from_directory(os.path.join(app_dir, 'static', 'font'), file_name)
 
+@app.route('/images/<path:file_name>')
+def serve_images_old(file_name):
+	return send_from_directory(os.path.join(app_dir, 'static', 'img'), file_name)
+
 
 @app.route('/font-awesome/<path:file_name>')
 def serve_font_awesome(file_name):
 	return send_from_directory(os.path.join(app_dir, 'static', 'font-awesome'), file_name)
+
+@app.route('/favicon.ico')
+def favicon():
+	return send_from_directory(os.path.join(app_dir, 'static', 'img'), 'favicon.ico')
 
 @app.route('/<page>')
 def main_route(page):
