0105e71da241232adb9601fb2e71a9866b1e027f
brianfisher <brian.charles.fisher@gmail.com>
2017-02-07 16:55:29 -0500

    Support for PC in master

--- app/views.py
+++ app/views.py
@@ -11,33 +11,33 @@ app_dir = os.path.abspath(os.path.dirname(__file__))
 #					
 ########################################################
 
-# The following should contain a comprehensive list of languages and pages
-# These are used to validate incoming URLs
-# languages = ['en', 'jp']
-# categories = ['solutions', 'verticals']
-# pages = ['base', 'b2b', 'email-marketing', 'lead-management', 'consumer-marketing', 
-# 		 'customer-base-marketing', 'mobile-marketing', 'higher-education',
-# 		 'financial-services', 'healthcare']
-
-pages = ['base', 'custombase','travel','healthcare','technology','manufacturing',
-		'highered','assetmgmt','financialservices','facebook','economist',
-		'hparameswaran']
-
-
-
+pages = [
+  'base',
+  'custombase',
+  'travel',
+  'healthcare',
+  'technology',
+  'manufacturing',
+	'highered',
+  'assetmgmt',
+  'financialservices',
+  'facebook',
+  'economist',
+  'predictive-content',
+	'hparameswaran'
+]
 
 @app.route('/')
 def no_language():
 	return redirect('/technology')
-	
+
 @app.route('/facebook')
 def facebook():
  	return send_from_directory(os.path.join(app_dir, 'templates'), 'facebook.html')
 
 @app.route('/economist')
 def economist():
- 	return send_from_directory(os.path.join(app_dir, 'templates'), 'economist.html') 	
-
+ 	return send_from_directory(os.path.join(app_dir, 'templates'), 'economist.html')
 
 @app.route('/js/<path:file_name>')
 def serve_js(file_name):
@@ -45,7 +45,7 @@ def serve_js(file_name):
 
 @app.route('/img/<path:file_name>')
 def serve_images(file_name):
-	return send_from_directory(os.path.join(app_dir, 'static', 'img'), file_name) #'static/images/', file_name)
+	return send_from_directory(os.path.join(app_dir, 'static', 'img'), file_name)
 
 @app.route('/css/<path:file_name>')
 def serve_css(file_name):
@@ -55,7 +55,6 @@ def serve_css(file_name):
 def serve_fonts(file_name):
 	return send_from_directory(os.path.join(app_dir, 'static', 'font'), file_name)
 
-
 @app.route('/font-awesome/<path:file_name>')
 def serve_font_awesome(file_name):
 	return send_from_directory(os.path.join(app_dir, 'static', 'font-awesome'), file_name)
@@ -63,8 +62,8 @@ def serve_font_awesome(file_name):
 @app.route('/<page>')
 def main_route(page):
 	if page in pages:
-		return render_template(page+'.html',
-								title='test',
-								category= 'travel')
+		return render_template(page + '.html')
 
-
+@app.route('/predictive-assets/<asset>')
+def predictive_asset(asset):
+  return render_template('predictive-assets.html', content=asset)
