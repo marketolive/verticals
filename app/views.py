61c19126f002930a241652693f0df1c9c92590fa
brianfisher <brian.charles.fisher@gmail.com>
2017-04-13 18:16:48 -0400

    301 Redirect from http to https for verticals.marketolive.com

--- app/views.py
+++ app/views.py
@@ -57,6 +57,13 @@ pages = [
   'channel_engagement'
 ]
 
+@app.before_request
+def before_request():
+  if request.url.startswith('http://') and not request.url.startswith('http://verticals-dev.marketolive.com') and not request.url.startswith('http://localhost:5000'):
+    url = request.url.replace('http://', 'https://', 1)
+    code = 301
+    return redirect(url, code=code)
+
 @app.route('/')
 def no_language():
 	return redirect('/technology')
