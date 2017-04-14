9c4fadd24e39c564e1795a2fd745181872a1c921
brianfisher <brian.charles.fisher@gmail.com>
2017-04-14 10:56:40 -0400

    Fix

--- config.py
+++ config.py
@@ -1,6 +1,1 @@
-from flask import Flask
-from flask_sslify import SSLify
-
-if 'DYNO' in os.environ and not request.host != 'verticals-dev.marketolive.com'):
-  app = Flask(__name__)
-  sslify = SSLify(app, permanent=True)
+FORCE_SSL = True
