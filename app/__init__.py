01479a229ec0bc5b8fa90651f3dbc586fd502b70
brianfisher <brian.charles.fisher@gmail.com>
2017-04-14 11:18:14 -0400

    Fix

--- app/__init__.py
+++ app/__init__.py
@@ -3,8 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 app.config.from_object('config')
 
-from app import views
-
 if app.config['FORCE_SSL'] and not app.debug:
   from flask_sslify import SSLify
   sslify = SSLify(app)
+
+from app import views
