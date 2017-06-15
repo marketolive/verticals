29358304e750cb61f32ab06843062cb4ec9bd5d0
cjhickens <Carter@carters-mbp.marketo.net>
2017-06-15 16:00:29 -0700

    incorporating melanie's content

---
+++ app/static/js/modal-page.js
@@ -0,0 +1,8 @@
+$( document ).ready(function() {
+	console.log("carter hates my indenting");
+
+	$(".icon-image").hover(function() {
+	    console.log("test");
+	      $(this).toggleClass("hover");    
+	}); 
+});
