a463449b026a06d29a2dd37180ac905b09eb6cb2
cjhickens <Carter@Carters-MacBook-Pro.local>
2017-07-20 12:46:57 -0700

    adding more css changes to predictive content on reloaded page

--- app/static/js/reloaded.js
+++ app/static/js/reloaded.js
@@ -6,8 +6,9 @@ $(document).ready(function() {
     	
         var title = $(".rtp_rcmd2_title"); 
         var content_tl = $(".rtp_rcmd2_link_hidden h4")
-        if (title.length ) {
-		    title.addClass('remove-title');
+        if (title.length) {
+		    title.css('display', 'none');
+		    $('.RTP_RCMD2[data-rtp-id]').css('max-width', '831px');
 		    content_tl.addClass('rtp-content-title, mdl-card__title-text');
 		    $(".rtp_rcmd2_link_container").css({
 		    	'height': '40px',
@@ -22,18 +23,49 @@ $(document).ready(function() {
 		    	'font-family': "Roboto, Helvetica, Arial, sans-serif"
 		    });
 
-		    $(".rtp_rcmd2_item_inner").hover(function() {
-		    	/* Stuff to do when the mouse enters the element */
+		    $(".rtp_rcmd2_content_container").hover(function() {
+		    	$(".rtp_rcmd2_content_container").css('background-color', '#5A54A4');
 		    	
 		    }, function() {
-		    	/* Stuff to do when the mouse leaves the element */
+		    	$(".rtp_rcmd2_content_container").css('background-color', '#E2E2E2');
 		    });
-		    console.log("RCE Custom Styling Complete");
 		    clearInterval(handle);
 		    handle = 0;
 		}
 		else {
-			// console.log("help?");
+			console.log("RCE Content Not Loading");
 		}
     }
+
+    // $.ajax({
+    // 	url: 'https://sjrtp3.marketo.com/gw1/rtp/api/v1_2/rcmd/richmedia/trending.json?session_id=mktodemolivemaster&visitor_id=mktodemolivemaster&account_id=mktodemolivemaster&template_id=template1&visit_count=1&view_count=1&categories=Demo',
+    // 	type: 'GET',
+    // 	dataType: 'json'    	
+    // })
+    // .done(function(data) {
+    // 	console.log("success");
+    // 	console.log(data);
+    // })
+    // .fail(function() {
+    // 	console.log("error");
+    // })
+    // .always(function() {
+    // 	console.log("Fetching RCE Content");
+    // });
+    
 });
+
+// RCE Content 
+// rtp('set', 'rcmd', 'richmedia','template1',
+// {
+//   "rcmd.general.font.family" : "Roboto, Helvetica, Arial, sans-serif",
+//   "rcmd.title.text" : "RECOMMENDED CONTENT",
+//   "rcmd.cta.text" : "Read More",
+//   "rcmd.title.background.color" : "#4285F4",
+//   "rcmd.title.font.color" : "white",
+//   "rcmd.content.background.color" :"#E2E2E2",
+//   "rcmd.cta.background.color" : "#737373",
+//   "category": ["Financial Services"]
+// }
+// );
+// rtp('get','rcmd', 'richmedia');
